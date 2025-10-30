f1054e0cfbd541c23ebf0b6f04905ede1ae4860e
selcuksarikoz <benimpostahesabim@gmail.com>
2025-10-30 07:25:53 +0100

    feat: github

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 import { db } from "@/server/db";
 
@@ -35,6 +36,10 @@ export const authConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
   adapter: PrismaAdapter(db),
